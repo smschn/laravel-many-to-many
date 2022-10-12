d7b8a5f4498be819c42371e44b8851f744ff117d
Luca Somaschini <somas99@gmail.com>
2022-10-12 17:25:25 +0200

    Added comments

--- app/Post.php
+++ app/Post.php
@@ -21,7 +21,9 @@ class Post extends Model
     }
 
     // aggiungo la relazione MANY TO MANY con la tabella (e quindi il model) <tags>.
+    // la funzione ha il nome della tabella con cui sussiste la relazione.
     public function tags() {
         return $this->belongstoMany('App\Tag'); // ritorno il model della tabella con cui c'è la relazione.
+        // 'leggendo il codice' è come dire: ad un post appartengono più tag.
     }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -7,7 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     // aggiungo la relazione MANY TO MANY con la tabella (e quindi il model) <posts>.
+    // la funzione ha il nome della tabella con cui sussiste la relazione.
     public function posts() {
         return $this->belongstoMany('App\Post'); // ritorno il model della tabella con cui c'è la relazione.
+        // 'leggendo il codice' è come dire: ad un tag appartengono più post.
     }
 }
